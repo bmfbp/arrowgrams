955ace3d5f73b786b1298f2f3cc3c6b1670a2a73
Paul Tarvydas <guitarvydas@gmail.com>
2020-12-21 10:09:33 -0500

    wip: working on load-and-run.js

--- kernel/load-and-run.js
+++ kernel/load-and-run.js
@@ -1,10 +1,17 @@
 // runtime loader (not part of the kernel)
 fs = require ("fs");
 function load_schematic (filename) {
+    let top_schematic = "";
     fs.readFile (filename, function (err, text) {
 	if (err) throw err;
 	let obj = JSON.parse (text);
-	for item in obj {
+	for (item in obj) {
 	    kindTable[item.name] = item;
+	    if (item.itemKind === "graph") {
+		// parts are emitted in reverse order
+		// - top schematic is the last part emitted
+		top_schematic = item.name;  // overwrite, leaving top-most
+	    }
 	}
+    })
 }
